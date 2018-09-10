170bebe9a1433290208964c7f79f5e3737bf4446
BerryPi <naturalneopoint@gmail.com>
2018-09-09 22:28:00 -0400

    Added ability to remove party members from list.

--- easyInit.js
+++ easyInit.js
@@ -15,4 +15,23 @@ function addPartyMember() {
 	var newRow = partyTable.insertRow(party.length);
 	newRow.insertCell().innerText = name;
 	newRow.insertCell().innerText = initBonus;
+	// Adding the button to remove the party member. The index needs to be worked out here to prevent scoping issues.
+	var memberIndex = party.length - 1
+	var removeButtonCell = newRow.insertCell();
+	removeButtonCell.innerHTML = "<input type=\"button\" value=\"X\">";
+	removeButtonCell.getElementsByTagName('input')[0].onclick = function() {removePartyMember(memberIndex);};
+}
+
+// Removes a member from the list of party members
+function removePartyMember(index) {
+	party.splice(index, 1);
+	// The header occupies a row in the table.
+	partyTable.deleteRow(index + 1);
+	// Update the indices for the removal buttons. The final row has the text input boxes and has no removal button.
+	for (var i = index + 1; i < partyTable.rows.length - 1 ; i++) {
+		// The index of the row is 1 more than the index in the table due to the header row
+		var listIndex = i - 1;
+		var removeButton = partyTable.rows[i].cells[2].getElementsByTagName('input')[0];
+		removeButton.onclick = function () {removePartyMember(listIndex);};
+	}
 }
