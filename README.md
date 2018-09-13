dc9e88d57f2050534cba42b83cb42a0aba07cb5a
BerryPi <naturalneopoint@gmail.com>
2018-09-12 20:43:10 -0400

    Added fuctionality to add and remove enemies, fixed bug with removal from lists.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # WebEasyInit
 A web-based tool for tabletop RPGs to more easily determine attack orders in enounters with multiple enemies
 
-## TODO
-- Removing party members  
-- Add and remove enemies  
+## TODO 
 - Roll for initiative and determine attack order  
 - Editing information in attack order  
 - Highlighting character currently taking their turn  
--- easyInit.js
+++ easyInit.js
@@ -1,37 +1,58 @@
 // The array of party members. Each has a name and initiative bonus.
 var party = []
 // The array of enemies
+var enemies = []
 
 // Grabbing the HTML entities so it does not have to be done each time
-var partyTable = document.getElementById('party');
+var partyTable = document.getElementById("party");
+var enemyTable = document.getElementById("enemies")
 
 // Adds a member and their initiative bonus to the list of party members
 function addPartyMember() {
 	// Save the member's name and initiative bonus
-	var name = document.getElementById('partyNameBox').value;
-	var initBonus = document.getElementById('partyInitBox').value;
-	party.push({name: name, initBonus: initBonus})
+	var name = document.getElementById("partyNameBox").value;
+	var initBonus = document.getElementById("partyInitBox").value;
+	var member = {name: name, initBonus: initBonus};
+	party.push(member);
 	// Adding the data to the table just before the text fields
 	var newRow = partyTable.insertRow(party.length);
 	newRow.insertCell().innerText = name;
 	newRow.insertCell().innerText = initBonus;
 	// Adding the button to remove the party member. The index needs to be worked out here to prevent scoping issues.
-	var memberIndex = party.length - 1
+	var memberIndex = party.length - 1;
 	var removeButtonCell = newRow.insertCell();
 	removeButtonCell.innerHTML = "<input type=\"button\" value=\"X\">";
-	removeButtonCell.getElementsByTagName('input')[0].onclick = function() {removePartyMember(memberIndex);};
+	removeButtonCell.getElementsByTagName("input")[0].onclick = function() {removeFromTable(partyTable, party, member);};
+	// Go back to the first text box for easier adding
+	document.getElementById("partyNameBox").focus();
 }
 
-// Removes a member from the list of party members
-function removePartyMember(index) {
-	party.splice(index, 1);
+function addEnemies() {
+	// Save the input
+	var name = document.getElementById("enemyNameBox").value;
+	var initBonus = document.getElementById("enemyInitBox").value;
+	var enemyQty = document.getElementById("enemyQtyBox").value;
+	var enemyType = {name: name, initBonus: initBonus, qty: enemyQty};
+	enemies.push(enemyType);
+	// Displaying the new data in the table
+	var newRow = enemyTable.insertRow(enemies.length);
+	newRow.insertCell().innerText = name;
+	newRow.insertCell().innerText = initBonus;
+	newRow.insertCell().innerText = enemyQty;
+	// Adding the button to remove the enemy. The index needs to be worked out here to prevent scoping issues.
+	var enemyIndex = enemies.length - 1;
+	var removeButtonCell = newRow.insertCell();
+	removeButtonCell.innerHTML = "<input type=\"button\" value=\"X\">";
+	removeButtonCell.getElementsByTagName("input")[0].onclick = function() {removeFromTable(enemyTable, enemies, enemyType);};
+	// Go back to the first text box for easier adding
+	document.getElementById("enemyNameBox").focus();
+}
+
+// Removes an element from one of the array-backed tables. It is an invariant of the add functions that the order of elements in the arrays matches the order of the rows in the table.
+function removeFromTable(table, array, item) {
+	// Find the item in the array
+	var index = array.indexOf(item);
+	array.splice(index, 1);
 	// The header occupies a row in the table.
-	partyTable.deleteRow(index + 1);
-	// Update the indices for the removal buttons. The final row has the text input boxes and has no removal button.
-	for (var i = index + 1; i < partyTable.rows.length - 1 ; i++) {
-		// The index of the row is 1 more than the index in the table due to the header row
-		var listIndex = i - 1;
-		var removeButton = partyTable.rows[i].cells[2].getElementsByTagName('input')[0];
-		removeButton.onclick = function () {removePartyMember(listIndex);};
-	}
+	table.deleteRow(index + 1);
 }
